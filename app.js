25db709b39a91a58c681aca130c67193ca238ed0
Gospel011 <ugochukwugospeli111@gmail.com>
2024-04-11 18:19:27 +0100

    Setup initial project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const morgan = require('morgan');
+
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+module.exports = app;
